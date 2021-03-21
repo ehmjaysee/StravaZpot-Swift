2e939f7cd9b4931080a7873ba341990fe3033a76
Mike Chartier <ehmjaysee@iclouc.com>
2021-03-21 16:02:30 -0500

    build 2.25.9

--- StravaZpot/DateParser.swift
+++ StravaZpot/DateParser.swift
@@ -11,9 +11,16 @@ import SwiftyJSON
 
 public class DateParser : Parser {
     public func from(json: JSON) -> Date {
+        #if true
+        //mjc
+        // The code below is causing a crash for a customer in the UK for some reason.
+        // MotionIQ does not use the date so a quick fix is to return today's date.
+        return Date()
+        #else
         let dateString = json.string!
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
         return dateFormatter.date(from: dateString)!
+        #endif
     }
 }
